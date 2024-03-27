75019d54006e47d73eded96f1e2f1fea4f245de0
bfbrandhorst <bfbrandhorst@gmail.com>
2024-03-27 15:28:10 -0600

    created styling for h1 in CSS, work on search button functionality in HTML and JS

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,15 +1,37 @@
-const searchedEl = document.getElementById('searched-cities');
+//const searchedEl = document.getElementById('searched-cities');
+const citySearchBtn = document.querySelector("#searched-cities");
 
-function init() {
-    const searchedLocations = JSON.parse(localStorage.getItems('locations'));
 
-    if (searchedLocations.length === 0) {
+
+
+// function init() {
+//     const searchedLocations = JSON.parse(localStorage.getItems('locations'));
+
+//     if (searchedLocations.length === 0) {
+//         return;
+//     }
+
+//     for (const location of searchedLocations) {
+
+//     }
+// }
+
+// init();
+
+
+function searchCity(event) {
+    event.preventDefault();
+
+    const searchedEl = document.querySelector('#city').value;
+
+
+
+    if (!searchedEl) {
+        console.error('You need a search input value!');
         return;
     }
+    console.log("working");
 
-    for (const location of searchedLocations) {
-
-    }
 }
 
-init();
+citySearchBtn.addEventListener('submit', searchCity);
