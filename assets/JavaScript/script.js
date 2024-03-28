d21c20453656a2a93ac5c1d27b7de971a85a694a
bfbrandhorst <bfbrandhorst@gmail.com>
2024-03-28 12:06:46 -0600

    changes to html and JavaScript: containers, accessing weather data code added

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,5 +1,31 @@
+const weatherApiRootUrl = 'https://api.openweathermap.org';
+const weatherApiKey = "decfc900f74a3a313929a403ee57e25e";
+const searchHistory = [];
+
+
 //const searchedEl = document.getElementById('searched-cities');
+const todayContainer = document.querySelector(".today-weather");
+const forecastContainer = document.querySelector(".five-day");
+const searchHistoryContainer = document.querySelector("#history");
 const citySearchBtn = document.querySelector("#searched-cities");
+
+
+//History Display Functionality:
+function renderSearchHistory() {
+    searchHistoryContainer.innerHTML = "";
+
+    for (let i = searchHistory.length - 1; i >= 0; i--) {
+        const citySearchBtn = document.createElement('button');
+        citySearchBtn.setAttribute('type', 'button');
+        citySearchBtn.setAttribute('aria-controls', 'todaysWeather');
+        citySearchBtn.classList.add('history-citySearchBtn', 'citySearchBtn-history');
+
+        citySearchBtn.setAttribute('data-search', searchHistory[i]);
+        citySearchBtn.textContent = searchHistory[i];
+        searchHistoryContainer.append(citySearchBtn);
+
+    }
+}
 
 
 
@@ -18,20 +44,76 @@ const citySearchBtn = document.querySelector("#searched-cities");
 
 // init();
 
+//Seacrh Button Functionality:
+async function searchCity(city) {
+    console.log(city);
+    try {
 
-function searchCity(event) {
+        const response = await fetch(`${weatherApiRootUrl}/data/2.5/weather?q=${city}&units=imperial&appid=${weatherApiKey}`)
+        const data = await response.json()
+        //console.log(data)
+        const icon = data.weather[0].icon
+        const url = `https://openweathermap.org/img/wn/${icon}.png`
+        const todayWeather = `
+        <div> 
+            <h3>${data.name}</h3>
+            <p>temp: ${data.main.temp}</p>
+            <p>humidity: ${data.main.humidity}</p>
+            <p>wind-speed: ${data.wind.speed}</p>
+            <img src = "${url}"/>
+        </div>
+        
+        `;
+        todayContainer.innerHTML = todayWeather
+        const lat = data.coord.lat
+        const lon = data.coord.lon
+
+
+        const fiveDayResponse = await fetch(
+            `${weatherApiRootUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${weatherApiKey}`
+        )
+        const fiveDayData = await fiveDayResponse.json()
+
+        const filteredArray = fiveDayData.list.filter((day) => day.dt_txt.includes("12:00:00"))
+        console.log(filteredArray)
+
+        let fiveDayCard = ""
+
+        filteredArray.forEach((day) => {
+            console.log(day)
+            let date = new Date(day.dt_txt).toLocaleDateString().split(",")[0]
+            const fiveIcon = data.weather[0].icon
+            const fiveUrl = `https://openweathermap.org/img/wn/${fiveIcon}.png`
+            fiveDayCard += `
+            <div> 
+            <h3>${date}</h3>
+            <p>temp: ${day.main.temp}</p>
+            <p>humidity: ${day.main.humidity}</p>
+            <p>wind-speed: ${day.wind.speed}</p>
+            <img src = "${fiveUrl}"/>
+        </div>
+            
+            `;
+            forecastContainer.innerHTML = fiveDayCard
+        })
+    } catch (error) {
+        console.error("error fetching data", error)
+    }
+
+
+
+
+}
+
+citySearchBtn.addEventListener('submit', function (event) {
     event.preventDefault();
-
-    const searchedEl = document.querySelector('#city').value;
-
-
-
+    const searchedEl = document.querySelector('#city').value.trim();
     if (!searchedEl) {
         console.error('You need a search input value!');
         return;
     }
     console.log("working");
 
-}
+    searchCity(searchedEl)
 
-citySearchBtn.addEventListener('submit', searchCity);
+});
